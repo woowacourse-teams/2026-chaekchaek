db2f0e341b0852ef5bf11a28db1faf6850edf0c1
stardusty-lab <startdusty.lab@gmail.com>
2026-08-03 14:04:15 +0900

    [FE] chore(eslint): eslint 설정을 airbnb 규칙으로

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: 'detect' } },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // 항상 마지막
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { ts: 'never', tsx: 'never', js: 'never', jsx: 'never' },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
