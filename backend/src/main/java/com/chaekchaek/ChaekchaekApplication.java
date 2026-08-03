b238520df500a9f2eb1650f606a96b7d6e9abc1d
HyoYoonNam <dev.rudevico@gmail.com>
2026-08-03 15:34:15 +0900

    [BE] build: 프로젝트 초기 세팅

---
+++ backend/src/main/java/com/chaekchaek/ChaekchaekApplication.java
@@ -0,0 +1,13 @@
+package com.chaekchaek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaekchaekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaekchaekApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/chaekchaek/ChaekchaekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaekchaek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaekchaekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
