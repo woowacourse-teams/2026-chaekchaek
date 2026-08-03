fc233994344bb34154fdf76e08454bba6e090a3c
stardusty-lab <startdusty.lab@gmail.com>
2026-08-03 20:14:54 +0900

    [FE] chore(package.json): devServer 설정

--- frontend/apps/chaekchaek/webpack.config.js
+++ frontend/apps/chaekchaek/webpack.config.js
@@ -41,4 +41,10 @@ module.exports = {
       template: path.resolve(__dirname, 'public/index.html'),
     }),
   ],
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
 };
