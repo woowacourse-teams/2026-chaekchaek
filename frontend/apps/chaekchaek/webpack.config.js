b40fb3c4d0e09479095c5c76508a006b880994bb
stardusty-lab <startdusty.lab@gmail.com>
2026-08-03 20:25:37 +0900

    [FE] chore(package.json): react-refresh 연결

--- frontend/apps/chaekchaek/webpack.config.js
+++ frontend/apps/chaekchaek/webpack.config.js
@@ -1,50 +1,57 @@
 const path = require('node:path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: path.resolve(__dirname, 'src/main.tsx'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    clean: true,
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'swc-loader',
-          options: {
-            jsc: {
-              parser: {
-                syntax: 'typescript',
-                tsx: true,
-              },
-              transform: {
-                react: {
-                  runtime: 'automatic',
+module.exports = (_, argv) => {
+  const isDevelopment = argv.mode === 'development';
+
+  return {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/main.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    refresh: isDevelopment,
+                  },
                 },
               },
             },
           },
         },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public/index.html'),
-    }),
-  ],
-  devServer: {
-    port: 3000,
-    open: true,
-    hot: true,
-    historyApiFallback: true,
-  },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public/index.html'),
+      }),
+      isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean),
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      historyApiFallback: true,
+    },
+  };
 };
