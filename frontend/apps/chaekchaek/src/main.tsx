d0927ee30214302b8455c9055ce04fc542c22459
stardusty-lab <startdusty.lab@gmail.com>
2026-08-04 19:34:33 +0900

    [FE] chore(msw): msw 기본 파일 추가 및 실행 코드 추가

--- frontend/apps/chaekchaek/src/main.tsx
+++ frontend/apps/chaekchaek/src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import { worker } from './mocks/msw/browser';
+
 import { App } from './App';
 
 const container = document.getElementById('root');
 
 if (!container) throw new Error('root 요소를 찾을 수 없습니다.');
+
+await worker.start({
+  serviceWorker: {
+    url: `/mockServiceWorker.js`,
+  },
+});
 
 const root = createRoot(container);
 root.render(
