932d1142dfc3cf897023934184589067b2020d13
stardusty-lab <startdusty.lab@gmail.com>
2026-08-04 13:52:25 +0900

    [FE] chore(design-system): npx create-storybook@latest --type react --builder webpack5
    - cd packages/design-system

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,13 +8,7 @@ module.exports = {
     ecmaFeatures: { jsx: true },
   },
   settings: { react: { version: 'detect' } },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended', // 항상 마지막
-  ],
+  extends: ['airbnb', 'airbnb-typescript', 'plugin:jsx-a11y/recommended', 'plugin:react-hooks/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'import/extensions': [
