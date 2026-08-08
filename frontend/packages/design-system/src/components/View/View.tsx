833d368f3bd869340aad41ca7de27027d0a07636
stardusty-lab <startdusty.lab@gmail.com>
2026-08-09 00:59:39 +0900

    [FE] feat(View): 제네릭 타입의 기본값 삭제

--- frontend/packages/design-system/src/components/View/View.tsx
+++ frontend/packages/design-system/src/components/View/View.tsx
@@ -2,7 +2,7 @@ import type { ElementType } from 'react';
 
 import type { Props } from './';
 
-export const View = <T extends ElementType = 'div'>(props: Props<T>) => {
+export const View = <T extends ElementType>(props: Props<T>) => {
   const { as, ...restProps } = props;
   const Component = as || 'div';
 
--- frontend/packages/design-system/src/components/View/View.types.ts
+++ frontend/packages/design-system/src/components/View/View.types.ts
@@ -1,10 +1,10 @@
 import type { ElementType, ComponentPropsWithoutRef } from 'react';
 
-export type Props<T extends ElementType = 'div'> = {
+export type Props<T extends ElementType> = {
   as?: T;
 } & ComponentPropsWithoutRef<T>;
 
-export type PolymorphicProps<T extends ElementType = 'div', P = {}> = {
+export type PolymorphicProps<T extends ElementType, P = {}> = {
   as?: T;
 } & Omit<ComponentPropsWithoutRef<T>, keyof P> &
   P;
