2d928e44ac48326c46048beafae7f473b59bd5fe
stardusty-lab <startdusty.lab@gmail.com>
2026-08-08 21:50:32 +0900

    [FE] feat(View): View 용 className. 삭제

--- frontend/packages/design-system/src/components/View/View.tsx
+++ frontend/packages/design-system/src/components/View/View.tsx
@@ -2,13 +2,9 @@ import type { ElementType } from 'react';
 
 import type { Props } from './';
 
-const classnameDefault = 'ui-View';
-
 export const View = <T extends ElementType = 'div'>(props: Props<T>) => {
   const { as, ...restProps } = props;
   const Component = as || 'div';
 
-  const classname = classnameDefault;
-
-  return <Component className={classname} {...restProps} />;
+  return <Component {...restProps} />;
 };
