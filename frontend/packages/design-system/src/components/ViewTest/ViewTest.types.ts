bdac35bae8419934627b3c806491f08b43fc9095
stardusty-lab <startdusty.lab@gmail.com>
2026-08-08 21:58:21 +0900

    [FE] feat(View): ViewTest 에서 자신의 OwnProps 정의하여 ViewProps에 제네릭으로 넘김

--- frontend/packages/design-system/src/components/ViewTest/ViewTest.types.ts
+++ frontend/packages/design-system/src/components/ViewTest/ViewTest.types.ts
@@ -4,6 +4,8 @@ import type { Props as ViewProps } from '#internal/components/View';
 
 export type AS = 'div';
 
-export type OwnProps = {};
+export type OwnProps = {
+  variant: 'test';
+};
 
-export type Props<T extends ElementType> = ViewProps<T>;
+export type Props<T extends ElementType> = ViewProps<T, OwnProps>;
