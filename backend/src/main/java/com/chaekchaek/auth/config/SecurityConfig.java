b41dce846aa19070fea8b9948c8826249a2181f6
snowshower <wonsb25@gmail.com>
2026-08-11 18:53:15 +0900

    [BE] feat: OAuth2 로그인 성공/실패 핸들러 추가 및 프론트 리다이렉트 처리
    Signed-off-by: snowshower <wonsb25@gmail.com>

--- backend/src/main/java/com/chaekchaek/auth/config/SecurityConfig.java
+++ backend/src/main/java/com/chaekchaek/auth/config/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.chaekchaek.auth.config;
 
+import com.chaekchaek.auth.handler.OAuth2AuthenticationFailureHandler;
+import com.chaekchaek.auth.handler.OAuth2AuthenticationSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,8 +12,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    public SecurityConfig(
+            OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler,
+            OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler
+    ) {
+        this.oAuth2AuthenticationSuccessHandler = oAuth2AuthenticationSuccessHandler;
+        this.oAuth2AuthenticationFailureHandler = oAuth2AuthenticationFailureHandler;
+    }
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
         return http
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
@@ -22,7 +36,10 @@ public class SecurityConfig {
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                        .failureHandler(oAuth2AuthenticationFailureHandler)
+                )
                 .build();
     }
 }
