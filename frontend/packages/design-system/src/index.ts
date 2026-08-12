596cde02885f684a44e00c300567ffb8f10bd6db
stardusty-lab <startdusty.lab@gmail.com>
2026-08-12 20:47:46 +0900

    [FE] feat(design-system): Callout 컴포넌트 추가

--- frontend/packages/design-system/src/index.ts
+++ frontend/packages/design-system/src/index.ts
@@ -1,2 +1,5 @@
 export { Avatar } from './components/Avatar';
 export type { Props as AvatarProps } from './components/Avatar';
+
+export { Callout } from './components/Callout';
+export type { Props as CalloutProps } from './components/Callout';
