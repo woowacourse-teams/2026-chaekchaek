0999f8c4b9c72f3ee3ced48cd732df864944a7e8
stardusty-lab <startdusty.lab@gmail.com>
2026-08-12 20:20:51 +0900

    [FE] chore(design-system): png, jgp, svg 타입 정의

--- frontend/packages/design-system/src/types/style.d.ts
+++ frontend/packages/design-system/src/types/style.d.ts
@@ -1,1 +1,22 @@
-declare module '*.css' {}
+declare module '*.module.css' {
+  const styles: Record<string, string>;
+  export default styles;
+}
+
+declare module '*.png' {
+  const src: string;
+
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+
+  export default src;
+}
