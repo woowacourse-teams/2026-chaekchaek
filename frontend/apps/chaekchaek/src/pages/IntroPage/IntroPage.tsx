8b5afbc1ad6601ea8139e4dd4813d15584e4fbf4
stardusty-lab <startdusty.lab@gmail.com>
2026-08-17 17:27:57 +0900

    [FE] feat(IntroPage): 빈 페이지 컴포넌트 연결

---
+++ frontend/apps/chaekchaek/src/pages/IntroPage/IntroPage.tsx
@@ -0,0 +1,3 @@
+export const IntroPage = () => {
+  return null;
+};
--- frontend/apps/chaekchaek/src/routes/routes.tsx
+++ frontend/apps/chaekchaek/src/routes/routes.tsx
@@ -2,6 +2,8 @@ import type { ReactNode } from 'react';
 
 import { ROUTES } from '@/constants/routes';
 
+import { IntroPage } from '@/pages/IntroPage';
+
 interface Route {
   path: string;
   element: ReactNode;
@@ -15,6 +17,10 @@ const routes: Route[] = [
   {
     path: ROUTES.HOME,
     element: <DummyPage />,
+  },
+  {
+    path: ROUTES.INTRO,
+    element: <IntroPage />,
   },
 ];
 
