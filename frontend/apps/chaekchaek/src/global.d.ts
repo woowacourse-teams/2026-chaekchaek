dd1ad76c4cdbafd5334657f8151cdab64e199725
stardusty-lab <startdusty.lab@gmail.com>
2026-08-17 17:29:25 +0900

    [FE] chore(env): @chaekchaek/web 에 css type 추가

--- frontend/apps/chaekchaek/src/global.d.ts
+++ frontend/apps/chaekchaek/src/global.d.ts
@@ -3,3 +3,8 @@ declare const __DEV__: boolean;
 declare const process: {
   env: NodeJS.ProcessEnv;
 };
+
+declare module '*.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
