889b71a9cab41ce79d85bb4a1327cb783d692d7f
stardusty-lab <startdusty.lab@gmail.com>
2026-08-19 00:36:52 +0900

    [FE] feat(books): BooksPage tsx template 렌더링

--- frontend/apps/chaekchaek/src/pages/BooksPage/BooksPage.tsx
+++ frontend/apps/chaekchaek/src/pages/BooksPage/BooksPage.tsx
@@ -1,3 +1,69 @@
+import { Layout } from '@chaekchaek/design-system';
+import { Header } from '@chaekchaek/design-system';
+import { Main } from '@chaekchaek/design-system';
+
+import { Split } from '@chaekchaek/design-system';
+import { OptionList } from '@chaekchaek/design-system';
+import { Title } from '@chaekchaek/design-system';
+import { List } from '@chaekchaek/design-system';
+import { ImgBox } from '@chaekchaek/design-system';
+// import DummyImgImgBox from '../../ImgBox/imgs/dummy.png';
+import { Button } from '@chaekchaek/design-system';
+import { Input } from '@chaekchaek/design-system';
+
 export const BooksPage = () => {
-  return null;
+  return (
+    <Layout>
+      <Header />
+      <Main>
+        <Split>
+          <Split.Side>
+            <Title
+              level="page"
+              orientation="vertical"
+              trailing={
+                <>
+                  <Input block />
+                </>
+              }
+            >
+              책 찾기
+            </Title>
+            <OptionList
+              title="필터"
+              value="all"
+              options={[
+                { value: 'all', text: '전체' },
+                { value: 'novel', text: '소설' },
+              ]}
+            />
+          </Split.Side>
+          <Split.Content>
+            <Title level="main" trailing={<></>}>
+              '마션' 검색 결과
+            </Title>
+            <List>
+              {Array.from({ length: 5 }).map(() => {
+                return (
+                  <List.Item>
+                    <List.Item.Leading>
+                      <ImgBox img={''} />
+                    </List.Item.Leading>
+                    <List.Item.Content
+                      title="마션"
+                      content="앤디 위어 · 박아람 옮김"
+                      description="알에이치코리아 · SF · 2026 · 308쪽"
+                    />
+                    <List.Item.Trailing>
+                      <Button>Button</Button>
+                    </List.Item.Trailing>
+                  </List.Item>
+                );
+              })}
+            </List>
+          </Split.Content>
+        </Split>
+      </Main>
+    </Layout>
+  );
 };
