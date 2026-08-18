de2e15e492afa9ea6495a62fab7eb83731391d73
stardusty-lab <startdusty.lab@gmail.com>
2026-08-18 21:12:49 +0900

    [FE] feat(msw): open api spec 문서로부터 handler 생성

--- frontend/apps/chaekchaek/src/mocks/msw/handlers.ts
+++ frontend/apps/chaekchaek/src/mocks/msw/handlers.ts
@@ -1,60 +1,13 @@
-import { http, HttpResponse } from 'msw';
+// import { http, HttpResponse } from 'msw';
 
-export const handlers = [
-  http.get('/health', () => {
-    return HttpResponse.json({ ok: 'health' }, { status: 200 });
-  }),
-];
+// export const handlers = [
+//   http.get('/health', () => {
+//     return HttpResponse.json({ ok: 'health' }, { status: 200 });
+//   }),
+// ];
 
-// import { fromOpenApi } from '@mswjs/source/open-api';
+import { fromOpenApi } from '@mswjs/source/open-api';
 
-// const openapiSpec = {
-//   openapi: '3.0.0',
-//   info: {
-//     version: '1.0.0',
-//     title: 'project API docs',
-//     description: 'project API docs',
-//     license: {
-//       name: 'project',
-//     },
-//   },
-//   tags: [
-//     {
-//       name: 'health',
-//       description: 'health check 관련 API',
-//     },
-//   ],
-//   servers: [
-//     {
-//       url: '/',
-//     },
-//   ],
-//   paths: {
-//     '/health': {
-//       get: {
-//         operationId: 'getHealth',
-//         tags: ['health'],
-//         description: 'health',
-//         responses: {
-//           '200': {
-//             description: 'health',
-//             content: {
-//               'application/json': {
-//                 schema: {
-//                   type: 'object',
-//                   properties: {
-//                     ok: {
-//                       type: 'boolean',
-//                     },
-//                   },
-//                 },
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   },
-// };
+import openapiSpec from '@/services/open-api-spec/openapi3.json';
 
-// export const handlers = await fromOpenApi(JSON.parse(JSON.stringify(openapiSpec)));
+export const handlers = await fromOpenApi(JSON.parse(JSON.stringify(openapiSpec)));
