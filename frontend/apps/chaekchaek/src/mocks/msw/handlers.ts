9e8f4962bf17aebdd29c455a04a9695214fef4bd
stardusty-lab <startdusty.lab@gmail.com>
2026-08-19 13:45:19 +0900

    [FE] feat(msw): open api spec 의 server url 주소 환경변수값으로 변경

--- frontend/apps/chaekchaek/src/mocks/msw/handlers.ts
+++ frontend/apps/chaekchaek/src/mocks/msw/handlers.ts
@@ -6,8 +6,14 @@
 //   }),
 // ];
 
+import { ENV } from '@/configs/env';
+
 import { fromOpenApi } from '@mswjs/source/open-api';
 
 import openapiSpec from '@/services/open-api-spec/openapi3.json';
 
-export const handlers = await fromOpenApi(JSON.parse(JSON.stringify(openapiSpec)));
+const apis = JSON.parse(JSON.stringify(openapiSpec));
+
+apis.servers[0].url = ENV.APP_API_URL;
+
+export const handlers = await fromOpenApi(JSON.parse(JSON.stringify(apis)));
