33dc66bb79973557eab902d7a4155b851e588ff3
stardusty-lab <startdusty.lab@gmail.com>
2026-08-21 19:20:19 +0900

    [FE] feat(design-system): Pagination 스토리북 케이스 작성

--- frontend/packages/design-system/src/components/Pagination/Pagination.stories.tsx
+++ frontend/packages/design-system/src/components/Pagination/Pagination.stories.tsx
@@ -1,20 +1,78 @@
 import type { Meta, StoryObj } from '@storybook/react-webpack5';
+import { useArgs } from 'storybook/preview-api';
 
 import { Pagination } from './';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Pagination/Pagination',
   component: Pagination,
+  args: {
+    defaultPage: 1,
+    totalPages: 10,
+  },
 } satisfies Meta<typeof Pagination>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Example: Story = {
+export const Default: Story = {};
+
+export const SinglePage: Story = {
   args: {
     defaultPage: 1,
-    totalPages: 10,
+    totalPages: 1,
+  },
+};
+
+export const ShowAllPages: Story = {
+  args: {
+    defaultPage: 4,
+    totalPages: 7,
+  },
+};
+
+export const NearStart: Story = {
+  args: {
+    defaultPage: 3,
+    totalPages: 20,
+  },
+};
+
+export const Middle: Story = {
+  args: {
+    defaultPage: 10,
+    totalPages: 20,
+  },
+};
+
+export const NearEnd: Story = {
+  args: {
+    defaultPage: 18,
+    totalPages: 20,
+  },
+};
+
+export const LastPage: Story = {
+  args: {
+    defaultPage: 20,
+    totalPages: 20,
+  },
+};
+
+export const Controlled: Story = {
+  args: {
+    defaultPage: 10,
+    totalPages: 20,
+  },
+  render: function Render(args) {
+    const [{ defaultPage }, updateArgs] = useArgs();
+
+    return (
+      <Pagination
+        {...args}
+        defaultPage={defaultPage}
+        onChange={(page) => updateArgs({ defaultPage: page })}
+      />
+    );
   },
 };
