fe9dfe9d9e6c78040bb7b128c70467a19b081d92
stardusty-lab <startdusty.lab@gmail.com>
2026-08-21 21:54:08 +0900

    [FE] fix(auth): http client instance 사용해서 api 호출 for 토큰 갱신

--- frontend/apps/chaekchaek/src/services/apis/booksIsbn/fetcher.ts
+++ frontend/apps/chaekchaek/src/services/apis/booksIsbn/fetcher.ts
@@ -1,11 +1,11 @@
-import { requestAjax } from '@/services/core/http';
+import { instance } from '@/services/core/http';
 
 import type { GetBooksIsbnRequestDto, GetBooksIsbnResponseDto } from './dto';
 
 export const getBooksIsbn = async ({
   pathParams: [{ value: isbn }],
 }: GetBooksIsbnRequestDto): Promise<GetBooksIsbnResponseDto> => {
-  const response = await requestAjax('/api/v1/books/by-isbn', {
+  const response = await instance('/api/v1/books/by-isbn', {
     method: 'get',
     pathParams: [{ name: 'isbn', value: isbn }],
   });
--- frontend/apps/chaekchaek/src/services/apis/homePopularBooks/fetcher.ts
+++ frontend/apps/chaekchaek/src/services/apis/homePopularBooks/fetcher.ts
@@ -1,10 +1,10 @@
-import { requestAjax } from '@/services/core/http';
+import { instance } from '@/services/core/http';
 
 import type { GetHomePopularBooksRequestDto, GetHomePopularBooksResponseDto } from './dto';
 
 export const getHomePopularBooks =
   async ({}: GetHomePopularBooksRequestDto): Promise<GetHomePopularBooksResponseDto> => {
-    const response = await requestAjax('/api/v1/home/popular-books', {
+    const response = await instance('/api/v1/home/popular-books', {
       method: 'get',
     });
 
