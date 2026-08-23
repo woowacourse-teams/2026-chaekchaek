7774d65d9e3b0964892face975cdbb103a1d0f69
stardusty-lab <startdusty.lab@gmail.com>
2026-08-23 21:59:04 +0900

    [FE] feat(auth): Haeder 안 프로필 아이콘 클릭시 로그인으로 이동

--- frontend/apps/chaekchaek/src/frames/Header/ProfileButton.tsx
+++ frontend/apps/chaekchaek/src/frames/Header/ProfileButton.tsx
@@ -1,5 +1,7 @@
 import { createClassName } from '@chaekchaek/design-system';
 
+import { ENV } from '@/configs/env';
+
 import profileIcon from './imgs/header-action.svg';
 import styles from './Header.module.css';
 
@@ -7,11 +9,7 @@ import type { ProfileButtonProps } from './ProfileButton.types';
 
 const classnameDefault = 'frame-Header-ProfileButton';
 
-export const ProfileButton = ({
-  className,
-  type = 'button',
-  ...restProps
-}: ProfileButtonProps) => {
+export const ProfileButton = ({ className, type = 'button', ...restProps }: ProfileButtonProps) => {
   const classname = createClassName({
     styles,
     baseName: classnameDefault,
@@ -19,8 +17,18 @@ export const ProfileButton = ({
     className,
   });
 
+  const handleMove = () => {
+    window.location.href = `${ENV.APP_API_URL}/oauth2/authorization/google`;
+  };
+
   return (
-    <button type={type} className={classname} aria-label="프로필" {...restProps}>
+    <button
+      type={type}
+      className={classname}
+      aria-label="프로필"
+      onClick={handleMove}
+      {...restProps}
+    >
       <img src={profileIcon} alt="" />
     </button>
   );
