af06e4ae4ce08732fc9bd4dbdb6000f1669cfa73
stardusty-lab <startdusty.lab@gmail.com>
2026-08-23 22:09:39 +0900

    [FE] feat(auth): 로그인 유무로 redirect 시키는 Route 추가

---
+++ frontend/apps/chaekchaek/src/auth/ProtectedRoute.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import type { ReactNode } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import { useAuthContext } from '@/contexts/AuthContext/useAuthContext';
+import { ROUTES } from '@/constants/routes';
+
+type Props = {
+  children: ReactNode;
+};
+
+const ProtectedRoute = ({ children }: Props) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { isAuthenticated } = useAuthContext();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate(ROUTES.LOGIN, {
+        replace: true,
+        state: { from: location },
+      });
+    }
+  }, [navigate, location, isAuthenticated]);
+
+  if (!isAuthenticated) return null;
+
+  return children;
+};
+
+export { ProtectedRoute };
