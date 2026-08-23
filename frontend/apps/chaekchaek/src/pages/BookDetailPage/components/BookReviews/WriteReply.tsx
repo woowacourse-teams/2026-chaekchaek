35f44218df964585ba3dc9d2b6830217951a3eb7
stardusty-lab <startdusty.lab@gmail.com>
2026-08-23 17:06:42 +0900

    [FE] feat(WriteReply): 사용자 답글 달기 기능처리

--- frontend/apps/chaekchaek/src/pages/BookDetailPage/components/BookReviews/WriteReply.tsx
+++ frontend/apps/chaekchaek/src/pages/BookDetailPage/components/BookReviews/WriteReply.tsx
@@ -1,6 +1,18 @@
 import { Avatar, Button, Field, Input, Shell, Surface } from '@chaekchaek/design-system';
 
+import { useFormValues } from '@/hooks/useFormValues';
+
+import { validateReply } from './validator';
+import type { ReplyFormValues } from './validator';
+
 export const WriteReply = () => {
+  const { values, errors, onChange, isValid, valids } = useFormValues<ReplyFormValues>({
+    initialValues: {
+      content: '',
+    },
+    validate: validateReply,
+  });
+
   return (
     <Surface>
       <Shell>
@@ -11,9 +23,14 @@ export const WriteReply = () => {
           title={
             <Field>
               <Field.Content>
-                <Input block />
-                <Button variant="ghost">답글 남기기</Button>
+                <Input id="content" value={values.content} onChange={onChange} block />
+                <Button variant="ghost" disabled={!isValid}>
+                  답글 남기기
+                </Button>
               </Field.Content>
+              {!valids.content && (
+                <Field.Description>{errors.content[0]?.message}</Field.Description>
+              )}
             </Field>
           }
         />
