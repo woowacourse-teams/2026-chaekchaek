5bac12c8ebb4f78d60efa0f0da1b1b7a64f39b9b
stardusty-lab <startdusty.lab@gmail.com>
2026-08-23 21:36:11 +0900

    [FE] feat(auth): /oauth/callback 에서 계정정보 요청 및 redirect 처리

--- frontend/apps/chaekchaek/src/pages/OauthPage/OauthPage.tsx
+++ frontend/apps/chaekchaek/src/pages/OauthPage/OauthPage.tsx
@@ -1,9 +1,12 @@
 import { useCallback, useEffect } from 'react';
-
-import { ENV } from '@/configs/env';
+import { useNavigate } from 'react-router-dom';
 
 import { useLoadData } from '@/services/core/useLoadData';
 import { getMembersMe } from '@/services/apis/membersMe/repository';
+
+import { useAuthContext } from '@/contexts/AuthContext/useAuthContext';
+
+import { ROUTES } from '@/constants/routes';
 
 export const OauthPage = () => {
   const getMembersMeLoadData = useCallback(async () => {
@@ -14,6 +17,16 @@ export const OauthPage = () => {
   } = useLoadData({
     queryFn: getMembersMeLoadData,
   });
+
+  const { login } = useAuthContext();
+
+  const navigation = useNavigate();
+
+  useEffect(() => {
+    if (membersMeData) return login(membersMeData);
+
+    navigation(ROUTES.HOME);
+  }, [membersMeData]);
 
   return null;
 };
