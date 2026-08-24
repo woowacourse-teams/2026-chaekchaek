ab3cc57b148d5ce917c16cb239f08fc948212599
stardusty-lab <startdusty.lab@gmail.com>
2026-08-25 02:26:09 +0900

    [FE] feat(DeleteBooksDialog): 여러 책 삭제 api 호출

--- frontend/apps/chaekchaek/src/pages/LibraryPage/dialog/DeleteBooksDialog/DeleteBooksDialog.tsx
+++ frontend/apps/chaekchaek/src/pages/LibraryPage/dialog/DeleteBooksDialog/DeleteBooksDialog.tsx
@@ -1,19 +1,32 @@
 import { Button, ButtonStack, Dialog } from '@chaekchaek/design-system';
 
+import { postLibraryBulkDelete } from '@/services/apis/libraryBulkDelete/repository';
+import { useExecute } from '@/services/core/useExecute';
+
 import type { DeleteBooksDialogProps } from './DeleteBooksDialog.types';
 
-export const DeleteBooksDialog = ({ onClose }: DeleteBooksDialogProps) => {
+export const DeleteBooksDialog = ({ bookIds, onClose }: DeleteBooksDialogProps) => {
+  const { mutate: deleteBooks } = useExecute({
+    executeFn: postLibraryBulkDelete,
+  });
+
+  const handleDeleteBooks = async () => {
+    await deleteBooks({ bookIds });
+  };
+
   return (
     <Dialog onClose={onClose}>
       <Dialog.Container>
-        <Dialog.Header subTitle="선택한 1권의 책을 서재에서 삭제할까?">책 삭제</Dialog.Header>
+        <Dialog.Header subTitle={`선택한 ${bookIds.length}권의 책을 서재에서 삭제할까?`}>
+          책 삭제
+        </Dialog.Header>
 
         <Dialog.Footer>
           <ButtonStack>
             <Button type="button" variant="ghost" size="large" block onClick={onClose}>
               취소
             </Button>
-            <Button type="button" variant="primary" size="large" block>
+            <Button type="button" variant="primary" size="large" block onClick={handleDeleteBooks}>
               삭제
             </Button>
           </ButtonStack>
--- frontend/apps/chaekchaek/src/pages/LibraryPage/dialog/DeleteBooksDialog/DeleteBooksDialog.types.ts
+++ frontend/apps/chaekchaek/src/pages/LibraryPage/dialog/DeleteBooksDialog/DeleteBooksDialog.types.ts
@@ -1,3 +1,4 @@
 export type DeleteBooksDialogProps = {
+  bookIds: number[];
   onClose: () => void;
 };
