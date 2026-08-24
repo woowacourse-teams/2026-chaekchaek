2fdc6eea79084461f5fd1727f9bdbbcd11ddff61
HyoYoonNam <dev.rudevico@gmail.com>
2026-08-24 16:33:33 +0900

    [BE] feat(book): 검색 결과에 내 서재 등록 여부 추가

--- backend/src/test/java/com/chaekchaek/book/dto/BookSearchResponseTest.java
+++ backend/src/test/java/com/chaekchaek/book/dto/BookSearchResponseTest.java
@@ -29,7 +29,8 @@ class BookSearchResponseTest {
                 "국내도서>소설>과학소설",
                 "알에이치코리아(RHK)",
                 3,
-                5
+                5,
+                null
         );
         BookSearchResponse response = new BookSearchResponse(6, 2, List.of(book));
 
@@ -42,7 +43,7 @@ class BookSearchResponseTest {
         );
         assertThat(json.at("/items/0").propertyNames()).containsExactlyInAnyOrder(
                 "bookId", "title", "coverImageUrl", "authors", "translators", "publishedDate",
-                "isbn13", "category", "publisher", "reviewCount", "replyCount"
+                "isbn13", "category", "publisher", "reviewCount", "replyCount", "isRegisteredInMyLibrary"
         );
     }
 
