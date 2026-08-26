3fa0ae1383a7376b3e7e242580084754003d1959
stardusty-lab <startdusty.lab@gmail.com>
2026-08-26 19:51:01 +0900

    [FE] feat(design-system): Input 컴포넌트에 leading, trailing 케이스 추가

--- frontend/packages/design-system/src/components/Input/Input.tsx
+++ frontend/packages/design-system/src/components/Input/Input.tsx
@@ -10,11 +10,13 @@ import type { Props } from './';
 const classnameDefault = 'ui-Input';
 
 export const Input = <T extends ElementType>(props: Props<T>) => {
-  const { as = 'input', className, size, block, reverse, ...restProps } = props;
+  const { as = 'input', className, size, block, leading, trailing, reverse, ...restProps } = props;
 
   const modifiers = {
     size: size && styles?.[`size-${size}`],
     block: block && styles?.[`is-block`],
+    leading: Boolean(leading) && styles?.[`has-leading`],
+    trailing: Boolean(trailing) && styles?.[`has-trailing`],
     reverse: reverse && styles?.[`is-reverse`],
   };
 
@@ -27,7 +29,9 @@ export const Input = <T extends ElementType>(props: Props<T>) => {
 
   return (
     <div className={classname}>
+      {leading && <span className={styles.leading}>{leading}</span>}
       <View as={as} {...restProps} />
+      {trailing && <span className={styles.trailing}>{trailing}</span>}
     </div>
   );
 };
