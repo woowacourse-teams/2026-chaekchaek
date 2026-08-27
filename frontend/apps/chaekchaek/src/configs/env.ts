9587a2560ec358724e597317bd142d116f6c0a8c
stardusty-lab <startdusty.lab@gmail.com>
2026-08-27 20:23:18 +0900

    [FE] chore(setnry): sentry 설정을 위한 환경 변수 정의

--- frontend/apps/chaekchaek/src/configs/env.ts
+++ frontend/apps/chaekchaek/src/configs/env.ts
@@ -3,6 +3,7 @@ const ENV = {
   APP_URL: process.env.APP_URL,
   APP_API_URL: process.env.APP_API_URL,
   APP_GA_ID: process.env.APP_GA_ID,
+  APP_SENTRY_DSN: process.env.APP_SENTRY_DSN,
 } as const;
 
 export { ENV };
