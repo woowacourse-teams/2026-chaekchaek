54d5a6f7c37d2d20ab4bfef3c5734368bf77b680
stardusty-lab <startdusty.lab@gmail.com>
2026-08-29 01:04:30 +0900

    [FE] fix(auth): Header 안 프로필 클릭시 로그인 처리시의 주소 오류 수정

--- frontend/apps/chaekchaek/src/frames/Header/ProfileButton.tsx
+++ frontend/apps/chaekchaek/src/frames/Header/ProfileButton.tsx
@@ -7,6 +7,8 @@ import styles from './Header.module.css';
 
 import type { ProfileButtonProps } from './ProfileButton.types';
 
+const clientEnv = `client=${__DEV__ ? 'local' : 'dev'}`;
+
 const classnameDefault = 'frame-Header-ProfileButton';
 
 export const ProfileButton = ({ className, type = 'button', ...restProps }: ProfileButtonProps) => {
@@ -18,7 +20,7 @@ export const ProfileButton = ({ className, type = 'button', ...restProps }: Prof
   });
 
   const handleMove = () => {
-    window.location.href = `${ENV.APP_API_URL}/oauth2/authorization/google`;
+    window.location.href = `${ENV.APP_API_URL}/api/v1/auth/oauth2/google?${clientEnv}`;
   };
 
   return (
