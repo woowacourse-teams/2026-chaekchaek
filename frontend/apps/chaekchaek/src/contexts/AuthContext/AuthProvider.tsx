ebeaf0a270e8f13c1768a29114ee451612f92434
stardusty-lab <startdusty.lab@gmail.com>
2026-08-31 14:25:24 +0900

    [FE] feat(auth): 비로그인인 경우 guestToken 요청해서 메모리 state에 저장

--- frontend/apps/chaekchaek/src/contexts/AuthContext/AuthProvider.tsx
+++ frontend/apps/chaekchaek/src/contexts/AuthContext/AuthProvider.tsx
@@ -2,13 +2,16 @@ import { useState, useMemo, useCallback, useEffect } from 'react';
 
 import { getMembersMe } from '@/services/apis/membersMe/repository';
 import { useLoadData } from '@/services/core/useLoadData';
+import { postAuthGuestToken } from '@/services/apis/authGuestToken/repository';
+import { useExecute } from '@/services/core/useExecute';
 
 import { authContext } from './AuthContext';
-import type { Props, UserData } from './AuthContext.types';
+import type { Props, UserData, GuestData } from './AuthContext.types';
 
 export const AuthProvider = ({ children }: Props) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [user, setUser] = useState<UserData | null>(null);
+  const [guest, setGuest] = useState<GuestData | null>(null);
 
   const login = useCallback((userData: UserData) => {
     setIsAuthenticated(true);
@@ -24,11 +27,33 @@ export const AuthProvider = ({ children }: Props) => {
     queryFn: getMembersMeLoadData,
   });
 
+  const guestLogin = useCallback((guestData: GuestData) => {
+    setIsAuthenticated(false);
+    setGuest(guestData);
+  }, []);
+
+  const {
+    mutate: postAuthGuestTokenMutate,
+    status: { data: authGuestToken },
+  } = useExecute({
+    executeFn: postAuthGuestToken,
+  });
+
   useEffect(() => {
     if (membersMeData) return login(membersMeData);
+    else postAuthGuestTokenMutate({});
   }, [membersMeData]);
 
-  const value = useMemo(() => ({ isAuthenticated, user, login }), [isAuthenticated, user, login]);
+  useEffect(() => {
+    if (authGuestToken) {
+      guestLogin(authGuestToken);
+    }
+  }, [authGuestToken]);
+
+  const value = useMemo(
+    () => ({ isAuthenticated, user, login, guest, guestLogin }),
+    [isAuthenticated, user, login, guest, guestLogin],
+  );
 
   return <authContext.Provider value={value}>{children}</authContext.Provider>;
 };
