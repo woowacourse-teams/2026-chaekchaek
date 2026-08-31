13e2bd5772ead0d0333cb72d1ce005e41d7c7683
stardusty-lab <startdusty.lab@gmail.com>
2026-09-01 01:22:51 +0900

    [FE] refactor(api): requestHeaders 를 repository, fetcher 로 나눔

--- frontend/apps/chaekchaek/src/services/context/requestHeaders.ts
+++ frontend/apps/chaekchaek/src/services/context/requestHeaders.ts
@@ -2,14 +2,29 @@ export type RequestHeaders = {
   'X-Guest-Token'?: string;
 };
 
-export type RequestHeaderContext = {
+export type RepositoryRequestHeaderContext = {
   guestToken: string | undefined;
 };
 
-export const createRequestHeaders = (context?: RequestHeaderContext): Record<string, string> => {
+export const createRepositoryRequestHeaders = (
+  context?: RepositoryRequestHeaderContext,
+): Record<string, string> => {
   if (!context?.guestToken) return {};
 
   return {
     'X-Guest-Token': context.guestToken,
+  };
+};
+type FetcherHeaders = {
+  'X-Guest-Token': string | undefined;
+};
+
+export const createFetcherRequestHeaders = ({
+  'X-Guest-Token': guestToken,
+}: FetcherHeaders): Record<string, string> => {
+  if (!guestToken) return {};
+
+  return {
+    'X-Guest-Token': guestToken,
   };
 };
