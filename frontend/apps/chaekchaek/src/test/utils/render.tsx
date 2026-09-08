f022de7290be42b96c40596095e1c2d37f9caa8e
stardusty-lab <startdusty.lab@gmail.com>
2026-09-08 23:07:20 +0900

    [FE] test(renderProvider): authContext Provider를 renderProvider 에 포함시키기

--- frontend/apps/chaekchaek/src/test/utils/render.tsx
+++ frontend/apps/chaekchaek/src/test/utils/render.tsx
@@ -2,15 +2,32 @@ import type { ReactNode } from 'react';
 
 import { render } from '@testing-library/react';
 
+import { authContext } from '@/contexts/AuthContext';
+import type { AuthContextValue } from '@/contexts/AuthContext';
+
+const defaultAuthContextValue = {
+  isAuthenticated: false,
+  user: null,
+  updateAccount: () => {},
+  guest: null,
+  updateGuestAccount: () => {},
+};
+
 interface TestProviderProps {
   children: ReactNode;
   route?: string;
+  auth: AuthContextValue;
 }
 
-export const TestProvider = ({ children }: TestProviderProps) => {
-  return <>{children}</>;
+export const TestProvider = ({ children, auth = defaultAuthContextValue }: TestProviderProps) => {
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>;
 };
 
-export const renderProvider = (children: ReactNode) => {
-  return render(<TestProvider>{children}</TestProvider>);
+export const renderProvider = (
+  children: ReactNode,
+  { auth }: { auth: AuthContextValue } = {
+    auth: defaultAuthContextValue,
+  },
+) => {
+  return render(<TestProvider auth={auth}>{children}</TestProvider>);
 };
