75e3ce59d02027dd01acc0879f5990dd67716015
stardusty-lab <startdusty.lab@gmail.com>
2026-09-08 22:17:57 +0900

    [FE] test(validators): 불필요한 suite 삭제

--- frontend/apps/chaekchaek/src/utils/validators.test.ts
+++ frontend/apps/chaekchaek/src/utils/validators.test.ts
@@ -1,21 +1,19 @@
-import { suite, describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 import { isString } from './validators';
 
-suite('validators', () => {
-  describe('isString', () => {
-    it('문자열이면 true를 반환한다', () => {
-      expect(isString('string')).toBe(true);
-    });
+describe('isString', () => {
+  it('문자열이면 true를 반환한다', () => {
+    expect(isString('string')).toBe(true);
+  });
 
-    it.each([
-      ['숫자', 1],
-      [undefined, undefined],
-      [null, null],
-      ['객체', {}],
-      ['배열', []],
-    ])('%s 이면 false를 반환한다', (_, value) => {
-      expect(isString(value)).toBe(false);
-    });
+  it.each([
+    ['숫자', 1],
+    [undefined, undefined],
+    [null, null],
+    ['객체', {}],
+    ['배열', []],
+  ])('%s 이면 false를 반환한다', (_, value) => {
+    expect(isString(value)).toBe(false);
   });
 });
