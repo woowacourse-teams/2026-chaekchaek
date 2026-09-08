be8251939d3263d752a5e98502733e6284ab3ef3
stardusty-lab <startdusty.lab@gmail.com>
2026-09-09 01:34:47 +0900

    [FE] test(BooksPage): fixtures 데이터로 api 응답값 대체하여 테스트

--- frontend/apps/chaekchaek/src/pages/BooksPage/BooksPage.test.tsx
+++ frontend/apps/chaekchaek/src/pages/BooksPage/BooksPage.test.tsx
@@ -3,9 +3,16 @@ import userEvent from '@testing-library/user-event';
 
 import { describe, it, expect } from 'vitest';
 
+import { server } from '@/mocks/msw/server';
+import { http, HttpResponse } from 'msw';
+
+import { ENV } from '@/configs/env';
+
 import { renderProvider } from '@/test/utils/render';
 
 import { BooksPage } from './BooksPage';
+
+import { harrySearchPage1 } from './BooksPage.fixtures';
 
 describe('BooksPage', () => {
   it('기본 렌더링이 된다', () => {
@@ -15,13 +22,22 @@ describe('BooksPage', () => {
   });
 
   it('검색어를 입력하면 검색 결과를 보여준다', async () => {
+    server.use(
+      http.get(`${ENV.APP_API_URL}/api/v1/books`, () => {
+        return HttpResponse.json({
+          ...harrySearchPage1,
+        });
+      }),
+    );
+
     renderProvider(<BooksPage />);
 
     const user = userEvent.setup();
 
-    await user.type(screen.getByRole('textbox', { name: '책 검색' }), '마션');
+    await user.type(screen.getByRole('textbox', { name: '책 검색' }), '해리');
 
-    expect(await screen.findByText('마션')).toBeInTheDocument();
-    expect(await screen.findByText('앤디 위어')).toBeInTheDocument();
+    expect(await screen.findByText(/개소리에/)).toBeInTheDocument();
+
+    expect(await screen.findByText(/^해리 포터와 마법사의 돌 1$/)).toBeInTheDocument();
   });
 });
