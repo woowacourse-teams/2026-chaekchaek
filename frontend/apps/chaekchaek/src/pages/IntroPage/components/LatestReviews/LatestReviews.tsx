0ab366933e7f45f7e3a3f0c2e6126ef577418a2a
stardusty-lab <startdusty.lab@gmail.com>
2026-09-21 00:14:52 +0900

    [FE] feat(LatestReviews): 최근 감상에서 모두 보기 버튼 삭제

--- frontend/apps/chaekchaek/src/pages/IntroPage/components/LatestReviews/LatestReviews.tsx
+++ frontend/apps/chaekchaek/src/pages/IntroPage/components/LatestReviews/LatestReviews.tsx
@@ -20,11 +20,6 @@ export const LatestReviews = () => {
     <div className={styles['scene-latest-reviews']}>
       <div className={styles['latest-reviews-title']}>
         <h1>방금 남겨진 문장</h1>
-        <div className="right">
-          <Button shape="link" variant="ghost" trailing={<Icon.LongArrowRightIcon />}>
-            모두보기
-          </Button>
-        </div>
       </div>
 
       {data?.reviews.map((review, index) => {
