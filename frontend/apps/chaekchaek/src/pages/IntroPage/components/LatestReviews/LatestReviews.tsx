9c2c02f056660706d28badaffd2637c562b5aa4a
stardusty-lab <startdusty.lab@gmail.com>
2026-09-20 23:49:01 +0900

    [FE] feat(LatestReviews): 최근 감상 더미 데이터로 template 작성

--- frontend/apps/chaekchaek/src/pages/IntroPage/components/LatestReviews/LatestReviews.tsx
+++ frontend/apps/chaekchaek/src/pages/IntroPage/components/LatestReviews/LatestReviews.tsx
@@ -1,4 +1,5 @@
-import { Button, Icon } from '@chaekchaek/design-system';
+import { Avatar, Entry, Shell, ImgBox, Icon, Button } from '@chaekchaek/design-system';
+
 import styles from './LatestReviews.module.css';
 
 export const LatestReviews = () => {
@@ -12,6 +13,53 @@ export const LatestReviews = () => {
           </Button>
         </div>
       </div>
+
+      {Array.from({ length: 7 }).map((_) => {
+        return (
+          <Entry reverse line="top" spacing="large">
+            <Entry.Main>
+              <Entry.Header>
+                <Shell reverse>
+                  <Shell.Leading>
+                    <ImgBox size="small" img="" />
+                  </Shell.Leading>
+                  <Shell.Content
+                    title="title"
+                    content={
+                      <>
+                        <Avatar size="x-small" img={null} />
+                        Content
+                      </>
+                    }
+                  />
+                </Shell>
+              </Entry.Header>
+              <Entry.Body>
+                "기억은 우리가 과거와 맺는 관계의 이름이 아니라, 현재 우리가 누 구인지를 결정짓는
+                살아있는 풍경이다. 우리는 매 순간 기억을 통 해..."
+              </Entry.Body>
+              <Entry.Footer>
+                <Button
+                  shape="link"
+                  size="small"
+                  variant="ghost"
+                  leading={<Icon.HeartOffIcon color="secondary" />}
+                >
+                  20
+                </Button>
+                <Button
+                  shape="link"
+                  size="small"
+                  variant="ghost"
+                  leading={<Icon.CommentIcon color="secondary" />}
+                >
+                  20
+                </Button>
+              </Entry.Footer>
+            </Entry.Main>
+          </Entry>
+        );
+      })}
     </div>
   );
 };
