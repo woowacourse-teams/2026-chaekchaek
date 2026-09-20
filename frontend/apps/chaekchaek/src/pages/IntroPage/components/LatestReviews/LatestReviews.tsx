c945207f980c0b04d5b080c02a5fb982542cc092
stardusty-lab <startdusty.lab@gmail.com>
2026-09-20 23:45:16 +0900

    [FE] feat(LatestReviews): 최근 감상에 제목 추가

--- frontend/apps/chaekchaek/src/pages/IntroPage/components/LatestReviews/LatestReviews.tsx
+++ frontend/apps/chaekchaek/src/pages/IntroPage/components/LatestReviews/LatestReviews.tsx
@@ -1,5 +1,17 @@
-import './LatestReviews.css';
+import { Button, Icon } from '@chaekchaek/design-system';
+import styles from './LatestReviews.module.css';
 
 export const LatestReviews = () => {
-  return <div className="scene-latest-reviews">latest reviews</div>;
+  return (
+    <div className={styles['scene-latest-reviews']}>
+      <div className={styles['latest-reviews-title']}>
+        <h1>방금 남겨진 문장</h1>
+        <div className="right">
+          <Button shape="link" variant="ghost" trailing={<Icon.LongArrowRightIcon />}>
+            모두보기
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
