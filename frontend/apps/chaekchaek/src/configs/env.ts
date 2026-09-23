0c87e3028cf419e0e6b3d8927bf207c5f4882e18
stardusty-lab <startdusty.lab@gmail.com>
2026-09-23 18:03:19 +0900

    [FE] feat(env): APP_ENV 타입 안전성 보장

--- frontend/apps/chaekchaek/src/configs/env.ts
+++ frontend/apps/chaekchaek/src/configs/env.ts
@@ -1,10 +1,22 @@
+type AppEnv = 'local' | 'development' | 'production';
+
+const getAppEnv = (): AppEnv => {
+  const appEnv = process.env.APP_ENV;
+
+  if (appEnv !== 'local' && appEnv !== 'development' && appEnv !== 'production') {
+    throw new Error(`Invalid APP_ENV: ${appEnv}`);
+  }
+
+  return appEnv;
+};
+
 const ENV = {
   APP_NAME: process.env.APP_NAME,
   APP_URL: process.env.APP_URL,
   APP_API_URL: process.env.APP_API_URL,
   APP_GA_ID: process.env.APP_GA_ID,
   APP_SENTRY_DSN: process.env.APP_SENTRY_DSN,
-  APP_ENV: process.env.APP_ENV,
+  APP_ENV: getAppEnv(),
 } as const;
 
 export { ENV };
