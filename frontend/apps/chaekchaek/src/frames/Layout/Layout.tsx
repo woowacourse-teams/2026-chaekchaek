2303011b3f584b73c7bdc5372b7d72281be42ce9
stardusty-lab <startdusty.lab@gmail.com>
2026-09-23 20:59:30 +0900

    [FE] feat(Layout): APP_ENV 에 따라 구분하기 위해 상단에 라인 색 주기
    - 주황: 개발 운영 환경
    - 초록: 로컬 개발 환경

--- frontend/apps/chaekchaek/src/frames/Layout/Layout.tsx
+++ frontend/apps/chaekchaek/src/frames/Layout/Layout.tsx
@@ -3,16 +3,26 @@ import type { ElementType } from 'react';
 import { View } from '@chaekchaek/design-system';
 import { createClassName } from '@chaekchaek/design-system';
 
+import { ENV } from '@/configs/env';
+
 import styles from './Layout.module.css';
 
 import type { Props } from '.';
 
 const classnameDefault = 'frame-Layout';
 
+const envIndicators = {
+  production: '',
+  development: 'env-development',
+  local: 'env-local',
+};
+
 export const Layout = <T extends ElementType>(props: Props<T>) => {
   const { as = 'div', className, ...restProps } = props;
 
-  const modifiers = {};
+  const modifiers = {
+    env: styles[envIndicators[ENV.APP_ENV]],
+  };
 
   const classname = createClassName({
     styles,
