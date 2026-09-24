9d90f69c58b7a2f8a6e7689c80e232727737866a
stardusty-lab <startdusty.lab@gmail.com>
2026-09-24 18:29:01 +0900

    [FE] feat(design-system): IconButton 의 props 등을 Button과 맞춤

--- frontend/packages/design-system/src/components/IconButton/IconButton.stories.tsx
+++ frontend/packages/design-system/src/components/IconButton/IconButton.stories.tsx
@@ -4,18 +4,97 @@ import { IconButton } from './';
 
 import { ArrowRightIcon } from '../Icon/icons';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'IconButton/IconButton',
+  title: 'Button/IconButton',
   component: IconButton,
+  args: {
+    'aria-label': '다음으로 이동',
+    children: <ArrowRightIcon />,
+    shape: 'default',
+    variant: 'default',
+    size: 'medium',
+  },
+  argTypes: {
+    shape: {
+      control: 'inline-radio',
+      options: ['default', 'normal', 'link'],
+    },
+    variant: {
+      control: 'select',
+      options: [
+        'default',
+        'primary',
+        'secondary',
+        'accent',
+        'ghost',
+        'soft',
+        'danger',
+        'danger-weak',
+      ],
+    },
+    size: {
+      control: 'inline-radio',
+      options: ['small', 'medium', 'large'],
+    },
+  },
 } satisfies Meta<typeof IconButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Example: Story = {
+export const Default: Story = {};
+
+export const PrimaryVariant: Story = {
   args: {
-    children: <ArrowRightIcon />,
+    children: <ArrowRightIcon color="inverse" />,
+    variant: 'primary',
+  },
+};
+
+export const SecondaryVariant: Story = {
+  args: { variant: 'secondary' },
+};
+
+export const AccentVariant: Story = {
+  args: { variant: 'accent' },
+};
+
+export const GhostVariant: Story = {
+  args: { variant: 'ghost' },
+};
+
+export const SoftVariant: Story = {
+  args: { variant: 'soft' },
+};
+
+export const DangerVariant: Story = {
+  args: { variant: 'danger' },
+};
+
+export const DangerWeakVariant: Story = {
+  args: { variant: 'danger-weak' },
+};
+
+export const SmallSize: Story = {
+  args: { size: 'small' },
+};
+
+export const LargeSize: Story = {
+  args: { size: 'large' },
+};
+
+export const NormalShape: Story = {
+  args: { shape: 'normal' },
+};
+
+export const LinkShape: Story = {
+  args: { shape: 'link' },
+};
+
+export const Disabled: Story = {
+  args: {
+    'aria-disabled': true,
+    children: <ArrowRightIcon color="inverse" />,
+    variant: 'primary',
   },
 };
